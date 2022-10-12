061566e0b301190aa9d12f9106fd7badd02f0843
Aamir Yousuf <aamir.yousuf@cleararchhealth.com>
2022-10-12 12:01:52 -0500

    Add and serve  app store images locally.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const server = require('http').createServer(app);
@@ -7,6 +8,7 @@ const fs = require('fs');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 
 
 
